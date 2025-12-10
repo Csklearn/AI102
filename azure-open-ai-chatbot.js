f518ad34cb2ce465ec7f28cf1d4e20c99f637c03
Csklearn <csklearn0711@gmail.com>
2025-12-10 03:28:46 -0800

    Update azure-open-ai-chatbot.js- UPDATES

--- azure-open-ai-chatbot.js
+++ azure-open-ai-chatbot.js
@@ -1,14 +1,11 @@
-import "./styles.css";
-
+import { useState } from "react";
 import OpenAI from "openai";
 
-import { useEffect } from "react";
-
 const endpoint =
-  "https://ai-hub57517195654113577198.openai.azure.com/openai/v1";
+  "https://ai-hub57557612348975757251.openai.azure.com/openai/v1";
 const deployment_name = "gpt-4o";
 const api_key =
-  "open_ai_api_key";
+  "";
 
 const client = new OpenAI({
   baseURL: endpoint,
@@ -16,30 +13,71 @@ const client = new OpenAI({
   dangerouslyAllowBrowser: true,
 });
 
-export default function App() {
-  useEffect(() => {
-    async function main() {
-      const completion = await client.chat.completions.create({
-        messages: [
-          { role: "assistant", content: "Helpful AI assistant" },
-          {
-            role: "user",
-            content:
-              "Can you help me with recent AI development in software field",
-          },
-        ],
-        model: deployment_name,
-      });
+export default function ChatBot() {
+  const [messages, setMessages] = useState([]);
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  async function main(query) {
+    setLoading(true);
+    const completion = await client.chat.completions.create({
+      messages: [
+        { role: "developer", content: "You are helpful assistant." },
+        {
+          role: "user",
+          content: query,
+        },
+      ],
+      model: deployment_name,
+    });
+    setLoading(false);
+    setMessages((prev) => [
+      ...prev,
+      ...[
+        { role: "user", content: query },
+        {
+          role: "assistant",
+          content:
+            completion.choices[0].message?.content ||
+            "Network Issues. Please try again later",
+        },
+      ],
+    ]);
+    console.log(completion.choices[0].message?.content);
+  }
 
-      console.log(completion.choices[0].message?.content);
-    }
+  //   useEffect(() => {
+  //     main("Hi");
+  //   }, []);
 
-    main();
-  });
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div style={{border:'2px solid black'}}>
+        <div >
+        <h3>RDS CHAT BOT</h3>
+        </div>
+        <div style={{marginLeft:'5px',marginRight:'5px',border:'1px solid black',padding:'5px',height:'50dvh'}}>
+          {messages.map((message) => (
+            <div>
+              {message.role}-{message.content}
+            </div>
+          ))}
+          {loading && <div>Fetching data from LLM...</div>}
+        </div>
+        <div style={{padding:'5px'}}>
+          <textarea
+            onChange={(event) => setQuery(event.target.value)}
+            value={query}
+          ></textarea>
+          <button
+            onClick={async () => {
+              await main(query);
+              setQuery("");
+            }}
+          >
+            Send
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
