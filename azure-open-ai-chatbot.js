a270cef295373f9a622ad368979884b0f1a24a6f
Csklearn <csklearn0711@gmail.com>
2025-12-10 07:58:33 -0800

    Update azure-open-ai-chatbot.js

--- azure-open-ai-chatbot.js
+++ azure-open-ai-chatbot.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import OpenAI from "openai";
-
+import { data } from "./data";
+import { marked } from "marked";
+//import Markdown from "react-markdown";
 const endpoint =
   "https://ai-hub57557612348975757251.openai.azure.com/openai/v1";
 const deployment_name = "gpt-4o";
-const api_key =
-  "";
+const api_key = "";
 
 const client = new OpenAI({
   baseURL: endpoint,
@@ -14,14 +15,14 @@ const client = new OpenAI({
 });
 
 export default function ChatBot() {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState(data);
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(false);
   async function main(query) {
     setLoading(true);
     const completion = await client.chat.completions.create({
       messages: [
-        { role: "developer", content: "You are helpful assistant." },
+        { role: "assistant", content: "You are helpful assistant." },
         {
           role: "user",
           content: query,
@@ -51,19 +52,56 @@ export default function ChatBot() {
 
   return (
     <>
-      <div style={{border:'2px solid black'}}>
-        <div >
-        <h3>RDS CHAT BOT</h3>
+      <div
+        style={{
+          border: "2px solid black",
+          display: "flex",
+          flexDirection: "column",
+          rowGap: "0.5rem",
+        }}
+      >
+        <div>
+          <h3>RDS CHAT BOT</h3>
         </div>
-        <div style={{marginLeft:'5px',marginRight:'5px',border:'1px solid black',padding:'5px',height:'50dvh'}}>
+        <div
+          style={{
+            marginLeft: "5px",
+            marginRight: "5px",
+            border: "1px solid black",
+            padding: "5px",
+            height: "50dvh",
+            overflow: "auto",
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+          }}
+        >
           {messages.map((message) => (
-            <div>
-              {message.role}-{message.content}
-            </div>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: marked.parse(message.content, { async: false }),
+              }}
+              style={{
+                backgroundColor:
+                  message.role === "user" ? "lightblue" : "lightgray",
+                textAlign: "left",
+                justifyContent:
+                  message.role === "user" ? "flex-end" : "flext-start",
+                paddingLeft: "10px",
+                paddingRight: "10px",
+                minWidth: "100px",
+                marginLeft: message.role === "user" && "auto",
+                marginRight: message.role === "assistant" && "auto",
+                borderRadius: "0.25rem",
+              }}
+            ></div>
+            // <div>
+            //   <Markdown>{message.content}</Markdown>
+            // </div>
           ))}
           {loading && <div>Fetching data from LLM...</div>}
         </div>
-        <div style={{padding:'5px'}}>
+        <div style={{ padding: "5px" }}>
           <textarea
             onChange={(event) => setQuery(event.target.value)}
             value={query}
